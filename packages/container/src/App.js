81cea511d764b90b328ccba1c4e0feee4007f71d
Akhil Somaraj <akhil.somraj@loblaw.ca>
2023-07-03 12:04:08 -0400

    Added Routing to Container and marketing app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles'
-import MarketingApp from './components/MarketingApp'
-import { BrowserRouter } from 'react-router-dom'
-import Header from './components/Header'
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 const generateClassName = createGenerateClassName({
-    productionPrefix: 'co'
-})
+  productionPrefix: 'co',
+});
 
-function App() {
-    return (
+export default () => {
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
         <div>
-            <StylesProvider generateClassName={generateClassName}>
-                <BrowserRouter>
-                    <Header />
-                    <MarketingApp />
-                </BrowserRouter>
-            </StylesProvider>
+          <Header />
+          <MarketingApp />
         </div>
-    )
-}
-
-export default App
+      </StylesProvider>
+    </BrowserRouter>
+  );
+};
