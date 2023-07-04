7e9fb68b5d626add37ed4ffd21c6e9f93b0c7791
Akhil Somaraj <akhil.somraj@loblaw.ca>
2023-07-03 23:58:49 -0400

    Set up auth and set up pipeline

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { useState, lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
   StylesProvider,
   createGenerateClassName,
 } from '@material-ui/core/styles';
-
-import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
-
+import Progress from './components/Progress';
+const LazyAuthApp = lazy(() => import('./components/AuthApp'));
+const LazyMarketingApp = lazy(() => import('./components/MarketingApp'));
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
 
 export default () => {
+
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
-          <MarketingApp />
+          <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <LazyAuthApp onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/" component={LazyMarketingApp} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
